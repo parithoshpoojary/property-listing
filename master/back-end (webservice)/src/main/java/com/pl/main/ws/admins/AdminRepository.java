2f5307ff2c33224524fc1fb31d40164c5a9dbde2
parithoshpoojary <parithosh21129917@gmail.com>
2021-09-01 18:29:33 +0530

    Linked the backend file in README file

--- master/back-end (webservice)/src/main/java/com/pl/main/ws/admins/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pl.main.ws.admins;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepository extends CrudRepository<Admin, Integer> {
-
-}
--- master/back-end (webservice)/src/main/java/com/pl/main/ws/categoryclass/CategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pl.main.ws.categoryclass;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoriesRepository extends CrudRepository<Category, Integer> {
-
-}
